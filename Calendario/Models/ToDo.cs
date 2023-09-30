0f324117b1b278c37fdfd55eac3a7022b23b7025
Adrian Mesa <adrianmesasacasas@outlook.com>
2023-09-29 22:07:41 -0400

    Primer vistazo al calendario

---
+++ Calendario/Models/ToDo.cs
@@ -0,0 +1,10 @@
+﻿namespace Calendario.Models;
+
+public class ToDo
+{
+    public int IdToDo { get; set; }
+    public string Content { get; set; } = null!;
+    public DateTime? InitialDate { get; set; }
+    public DateTime? CloseDate { get; set; }
+    public DateTime? LastModifiedDate { get; set;}
+}
